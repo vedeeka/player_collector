08048fef81a97d019bbee2f9bf7c83e94025aab2
vedeeka <vedeekaparab9999@gmail.com>
2025-02-25 23:00:11 +0530

    Refactor authentication handling by replacing firebase_auth.dart with firebase_wrap.dart and updating main.dart imports

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:player_collector/firbase_authentication.dart';
+import 'package:player_collector/firebase_wrap.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
 
